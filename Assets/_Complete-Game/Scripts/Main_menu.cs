462b8534950e8969476e5f3df5b6c35b12b64bda
LAPTOP-1ARE4SOF\Thilak Raj <sound001>
2018-02-12 18:04:36 -0800

    first commit

---
+++ Assets/_Complete-Game/Scripts/Main_menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void Roll()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Shooter()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+
+    }
+
+
+}
